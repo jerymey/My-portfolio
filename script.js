20840c0dd62aed68def6ff761f3ce70e2d462b46
jery Mey <jerymey25@gmqil.com>
2023-06-30 11:17:00 +0100

    updated Js linters

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const hamIcon = document.getElementById("ham-icon");
-const menu = document.getElementById("menu");
-const closeIcon = document.getElementById("close-icon");
-
+const hamIcon = document.getElementById('ham-icon');
+const menu = document.getElementById('menu');
+const closeIcon = document.getElementById('close-icon');
 function openMenu() {
-    hamIcon.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
+    hamIcon.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
 }
 function closeMenu() {
-    hamIcon.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
+    hamIcon.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
 }
-
-hamIcon.addEventListener("click", openMenu);
-closeIcon.addEventListener("click", closeMenu);
+hamIcon.addEventListener('click', openMenu);
+closeIcon.addEventListener('click', closeMenu);
