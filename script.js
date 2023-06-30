98b079ad1abcbae1db6667a8227af08480b7044a
jery Mey <jerymey25@gmqil.com>
2023-06-30 15:30:52 +0100

    Fix linters

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const menu = document.getElementById('menu');
 const closeIcon = document.getElementById('close-icon');
 
 function openMenu() {
-    hamIcon.classList.toggle('hidden');
-    menu.classList.toggle('hidden');
+  hamIcon.classList.toggle('hidden');
+  menu.classList.toggle('hidden');
 }
 function closeMenu() {
-    hamIcon.classList.toggle('hidden');
-    menu.classList.toggle('hidden');
+  hamIcon.classList.toggle('hidden');
+  menu.classList.toggle('hidden');
 }
 hamIcon.addEventListener('click', openMenu);
 closeIcon.addEventListener('click', closeMenu);
